3c8e9e59206a1ce1f2c53064518a066561b92e3e
Tim <tim.weinert@gmail.com>
2020-01-20 16:06:09 +1100

    V1.02 document query selector

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 //window is the event target i.e. the user must press a key whilst on the page
 //function (e) - e is shorthand for Keyboard event (key is pressed)
 window.addEventListener('keydown', function (e) {
-    //keycode instead of actual key 
-    console.log(e.keyCode);
+    //set variable for audio
+    //document (html / DOM) querySelector (element and class / id)
+    //reference the audio element with a data-key = "65"
+    const audio = document.querySelector(`audio[data-key=${e.keyCode}]`)
 });
 
 
