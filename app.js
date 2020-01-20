5814720d67c3ebb14f8c01c6c09c965343326c50
Tim <tim.weinert@gmail.com>
2020-01-20 15:26:23 +1100

    V1.00 Keydown event listener and comments

---
+++ app.js
@@ -0,0 +1,9 @@
+//Add event listener for when a key is pressed
+//window is the event target i.e. the user must press a key whilst on the page
+//function (e) - e is shorthand for Keyboard event (key is pressed)
+window.addEventListener('keydown', function (e) {
+    //keycode instead of actual key 
+    console.log(e - keycode);
+});
+
+
